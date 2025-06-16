ad44cb3c90f3cf7aa0241804ace29ae855c48ca6
Merlord <jchaakma@gmail.com>
2025-06-16 20:32:16 +1200

    Add tile mapping fields to Biome

--- IslandGeneration/Biome.gd
+++ IslandGeneration/Biome.gd
@@ -5,3 +5,7 @@ class_name Biome
 @export var min_height: float = 1.0
 @export var color: Color = Color.WHITE
 @export var is_land: bool = true
+@export var biome_id: String = ""
+@export var layer_index: int = 0
+@export var tile_set: TileSet
+@export var terrain_id: int = 0
