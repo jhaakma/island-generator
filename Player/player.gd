c310a4284f4ea333eae51f3b4a6fc06cdd245cef
jhaakma <jchaakma@gmail.com>
2025-06-17 23:17:02 +1200

    Improve collision and movement

--- Player/player.gd
+++ Player/player.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 @export var rotation_speed: float = 3.0
 @export var drag: float = 0.95
 @export var max_speed: float = 1000.0
+@export var redirect_speed: float = 0.9 # Adjust this value for more/less steering effect
 
 func _ready() -> void:
     velocity = Vector2.ZERO
@@ -13,6 +14,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
     handle_rotation(delta)
     handle_thrust(delta)
+    redirect_velocity_towards_facing(delta)
     apply_drag()
     move_and_slide()
     handle_collisions()
@@ -36,6 +38,11 @@ func handle_thrust(delta: float) -> void:
         var direction := Vector2.UP.rotated(rotation)
         velocity -= direction * reverse_force * delta
         velocity = velocity.limit_length(max_speed)
+
+# Redirects a percentage of current velocity towards the ship's facing direction when turning
+func redirect_velocity_towards_facing(delta: float) -> void:
+    var facing = Vector2.UP.rotated(rotation)
+    velocity = velocity.lerp(facing * velocity.length(), redirect_speed * delta)
 
 func handle_collisions() -> void:
     pass
