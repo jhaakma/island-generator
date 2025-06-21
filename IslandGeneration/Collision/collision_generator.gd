3c8ade9d8d751a648859fdcf3a31c9744d80562b
jhaakma <jchaakma@gmail.com>
2025-06-21 18:10:21 +1200

    Improve collision, rendering

--- IslandGeneration/Collision/collision_generator.gd
+++ IslandGeneration/Collision/collision_generator.gd
@@ -12,7 +12,7 @@ func create_land_mask(height_map: Image) -> BitMap:
 
 
 func generate_collision_polygons(map_image: Image)-> Array[CollisionPolygon2D]:
-
+    print("Generating collision polygons with accuracy: ", accuracy)
     var land_mask: BitMap = create_land_mask(map_image)
     var rect := Rect2i(Vector2i.ZERO, map_image.get_size())
     var epsilon := 1.0 / accuracy
@@ -21,6 +21,20 @@ func generate_collision_polygons(map_image: Image)-> Array[CollisionPolygon2D]:
     var collision_polygons: Array[CollisionPolygon2D] = []
 
     for polygon in polygons:
+        # ignore small polygons
+
+        var min_length = INF
+        var max_length = 0.0
+        var circumference := 0.0
+        for point in polygon:
+            var next_point := polygon[(polygon.find(point) + 1) % polygon.size()]
+            var length := point.distance_to(next_point)
+            max_length = max(max_length, length)
+            min_length = min(min_length, length)
+            circumference += length
+        if circumference / polygon.size() <= 1.0:
+            print("Skipping polygon with average legnth <= 1.0")
+            continue
         var collision_polygon := CollisionPolygon2D.new()
         collision_polygon.polygon = polygon
         collision_polygon.position = Vector2.ZERO
